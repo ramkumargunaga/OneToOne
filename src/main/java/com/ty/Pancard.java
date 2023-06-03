52a33f6c485c75246627966449ac83ac7d33befb
ramkumargunaga <ramkumarpgunaga@gmail.com>
2023-06-03 12:47:23 +0530

    one to one

--- src/main/java/com/ty/Pancard.java
+++ src/main/java/com/ty/Pancard.java
@@ -1,5 +1,41 @@
 package com.ty;
 
 public class Pancard {
+	
+	private int id;
+	private String name;
+	private String address;
+	private long phone;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	
+	@Override
+	public String toString() {
+		return "Pancard [id=" + id + ", name=" + name + ", address=" + address + ", phone=" + phone + "]";
+	}
+	
 
 }
